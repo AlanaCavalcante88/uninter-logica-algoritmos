a4f1f664c6938209530a585f1a62286b2e8e2632
AlanaCavalcante88 <alanacristinaf@gmail.com>
2026-09-14 07:33:54 -0300

    add importando bibliotecas

---
+++ aula6-estrutura-de-dados/exe2-importando-bibliotecas.py
@@ -0,0 +1,15 @@
+# Importando bibliotecas
+# Importa ao seu programa novas funções
+# Elas podem ser predefinidas da linguagem, ou criadas por outra pessoa ou por você mesmo
+# Django é uma biblioteca -> usa para parte do frontend e backend
+# Primeiro tem que encontrar a versão do Python
+
+import math # aqui é o nome da biblioteca
+
+print(math.sqrt(9)) # aqui sempre coloca o nome da biblioteca.o nome da função
+
+# Pode também dar um nome para a biblioteca, por exemplo import math as m (m é o nome que está dando)
+
+# Se não quiser importar toda a biblioteca, e importar apenas a função, tem que fazer:
+# from math import sqrt
+# Nesse caso você está apenas chamando a função sqrt e isso evita problemas no seu programa
